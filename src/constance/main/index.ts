5e427a54c19b739b8571b51e1ea5ed65c5672fce
SxxHyun <shp35079@naver.com>
2022-01-03 13:40:52 +0900

    [MODIFY] select elements 객체 수정

--- src/constance/main/index.ts
+++ src/constance/main/index.ts
@@ -1,5 +1,5 @@
 export const SELECT_ELEMENTS = [
-  { name: '학년', template: '학년', num: 3 },
-  { name: '반', template: '반', num: 4 },
-  { name: '번호', template: '번', num: 21 },
+  { name: 'grade', placeholder: '학년', template: '학년', num: 3 },
+  { name: 'class', placeholder: '반', template: '반', num: 4 },
+  { name: 'number', placeholder: '번호', template: '번', num: 21 },
 ];
